623ea2a45f15b0c3fd4f6b7ef18a2f380ffb4c13
Nghiem Ba Hieu <hi3unb@gmail.com>
2019-07-02 00:16:42 +0700

    Initial release

--- metadata.rb
+++ metadata.rb
@@ -5,16 +5,20 @@ license 'Apache-2.0'
 description 'Installs/Configures googlechrome'
 long_description 'Installs/Configures googlechrome'
 version '0.1.0'
-chef_version '>= 13.0'
+chef_version '>= 12.14'
+
+supports 'windows'
+
+depends 'chrome', '~> 4.0.2'
 
 # The `issues_url` points to the location where issues for this cookbook are
 # tracked.  A `View Issues` link will be displayed on this cookbook's page when
 # uploaded to a Supermarket.
 #
-# issues_url 'https://github.com/<insert_org_here>/googlechrome/issues'
+issues_url 'https://github.com/hieunba/chef-googlechrome/issues'
 
 # The `source_url` points to the development repository for this cookbook.  A
 # `View Source` link will be displayed on this cookbook's page when uploaded to
 # a Supermarket.
 #
-# source_url 'https://github.com/<insert_org_here>/googlechrome'
+source_url 'https://github.com/hieunba/chef-googlechrome'
